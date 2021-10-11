50b61c9596437c59a8e8f506155f04334e3ba9ac
Nethrini9 <92314500+Nethrini9@users.noreply.github.com>
2021-10-11 20:04:18 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,30 @@
+class Swap
+{
+	public static void main(String arg[])	
+	{
+		int a[]={6,4,20,2};
+		int b[]={5,9,1,3};  
+
+		while(true)
+		{
+			int s1=0,s2=0;
+			int i,j;
+			for(i=0;i<=3;i++)
+			{
+				s1=s1+a[i];
+			}
+
+			for(j=0;j<=3;j++)
+			{
+				s2=s2+b[j];
+			}
+
+			break;
+			
+			
+		}
+	}
+}
+			
+
+			
